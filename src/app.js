0cdf1982cbbb20396ad1c56bb58fdacd66a02294
Vinicius Canuto <vcanutodev@gmail.com>
2020-05-08 22:19:01 -0300

    Feito CRUD, likes e validações

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,86 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Repositório não existe"});
+  }
+
+  const {title, url, techs, likes} = request.body;
+
+  if(likes){
+    return response.status(400).json({likes: 0});
+  }
+  const oldLikes = repositories[repositoryIndex].likes;
+
+  const repository ={
+    id,
+    title,
+    url,
+    techs,
+    likes: oldLikes
+  };
+
+  repositories[repositoryIndex] = repository;
+  
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json();
+  };
+
+  repositories.splice(repositoryIndex,1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json();
+  };
+  
+  const repository = repositories[repositoryIndex];
+
+  repository.likes++;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
